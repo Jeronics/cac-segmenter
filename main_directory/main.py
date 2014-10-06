4cfb236ebce6ff409510e86c15042b05228caf56
Jeroni Carandell Saladich <jeronicarandellsaladich@Jeronis-MacBook-Pro.local>
2014-10-06 12:54:28 +0200

    Improve main

--- main_directory/main.py
+++ main_directory/main.py
@@ -1,20 +1,16 @@
 __author__ = 'jeronicarandellsaladich'
-
 import sys
 import time
 from utils import *
 
+if __name__ == "__main__":
+    start=time.time()
 
-#Just measuring time!
-start=time.time()
+    # Read inputs, return numpy arrays for cage/s and mask.
+    mask_file, init_cage_file, curr_cage_file=get_inputs(sys.argv)
 
-mask_file, init_cage_file, curr_cage_file=get_inputs(sys.argv)
+    # THE END
 
-
-# THE END
-
-
-
-#Just measuring time!
-end=time.time()
-print end-start
+    # Time elapsed
+    end=time.time()
+    print end-start
