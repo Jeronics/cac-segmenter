3ba08b8dbbc5e6cd8dafb0e21f2b53b27d8145d7
Jeroni Carandell Saladich <jeronicarandellsaladich@Jeronis-MacBook-Pro.local>
2014-10-06 12:51:25 +0200

    Clean main file

--- main_directory/main.py
+++ main_directory/main.py
@@ -8,48 +8,7 @@ from utils import *
 #Just measuring time!
 start=time.time()
 
-
-
-#./cac model imatge mascara caixa_init [caixa_curr]
-if (len(sys.argv) != 6 and len(sys.argv) != 5 ):
-    print 'Wrong Use!!!! Expected Input ' +sys.argv[0] + ' model(int) image(int) mask(int) init_cage(int) [curr_cage(int)]'
-    sys.exit(1)
-
-for arg in sys.argv:
-    print arg
-model = sys.argv[0]
-mask = int(sys.argv[1])
-init_cage= int(sys.argv[2])
-if len(sys.argv) == 6:
-    curr_cage = int(sys.argv[5]);
-else:
-    curr_cage = None
-
-
-# PATHS
-test_path = r'../test/elefant/'
-mask_num = '%(number)02d' % {"number": mask}
-init_cage_name = '%(number)02d' % {"number": init_cage}
-curr_cage_name = '%(number)02d' % {"number": curr_cage}
-
-mask_name = test_path + 'mask_'+mask_num+'.pgm'
-init_cage_name = test_path + 'cage_'+init_cage_name+'.txt'
-curr_cage_name = test_path + 'cage_'+curr_cage_name+'.txt'
-
-# LOAD Cage and Mask
-
-mask_file = read_pgm(mask_name,byteorder='>')
-
-mask_file = read_pgm(mask_name,byteorder='>')
-init_cage_file = np.loadtxt(init_cage_name, float)
-curr_cage_file = np.loadtxt(curr_cage_name, float)
-
-print init_cage_file
-print mask_file
-
-
-
-
+mask_file, init_cage_file, curr_cage_file=get_inputs(sys.argv)
 
 
 # THE END
--- main_directory/utils.py
+++ main_directory/utils.py
@@ -4,6 +4,8 @@ import re
 import numpy as np
 from matplotlib import pyplot
 
+
+
 def read_pgm(filename, byteorder='>'):
     """Return image data from a raw PGM file as numpy array.
 
@@ -26,6 +28,42 @@ def read_pgm(filename, byteorder='>'):
                             offset=len(header)
                             ).reshape((int(height), int(width)))
 
+def get_inputs(arguments):
+    """Return cages and images from input as numpy array.
+        Specification:  ./cac model imatge mascara caixa_init [caixa_curr]
+    """
+
+    if (len(arguments) != 6 and len(arguments) != 5 ):
+        print 'Wrong Use!!!! Expected Input ' +sys.argv[0] + ' model(int) image(int) mask(int) init_cage(int) [curr_cage(int)]'
+        sys.exit(1)
+
+    for arg in arguments:
+        print arg
+    model = arguments[0]
+    mask = int(arguments[1])
+    init_cage= int(arguments[2])
+    if len(arguments) == 6:
+        curr_cage = int(arguments[5]);
+    else:
+        curr_cage = None
+
+
+    # PATHS
+    test_path = r'../test/elefant/'
+    mask_num = '%(number)02d' % {"number": mask}
+    init_cage_name = '%(number)02d' % {"number": init_cage}
+    curr_cage_name = '%(number)02d' % {"number": curr_cage}
+
+    mask_name = test_path + 'mask_'+mask_num+'.pgm'
+    init_cage_name = test_path + 'cage_'+init_cage_name+'.txt'
+    curr_cage_name = test_path + 'cage_'+curr_cage_name+'.txt'
+
+    # LOAD Cage/s and Mask
+    mask_file = read_pgm(mask_name,byteorder='>')
+    init_cage_file = np.loadtxt(init_cage_name, float)
+    curr_cage_file = np.loadtxt(curr_cage_name, float)
+
+    return mask_file, init_cage_file, curr_cage_file
 
 
 
