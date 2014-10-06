691aa736d0bb9757b856945a090073a1359e5aee
Jeroni Carandell Saladich <jeronicarandellsaladich@Jeronis-MacBook-Pro.local>
2014-10-06 12:00:22 +0200

    Adapt main.py to entry of test to numpy

--- main_directory/main.py
+++ main_directory/main.py
@@ -1,5 +1,6 @@
 __author__ = 'jeronicarandellsaladich'
 import sys
+import numpy as np
 
 #./cac model imatge mascara caixa_init [caixa_curr]
 if (len(sys.argv) != 6 and len(sys.argv) != 5 ):
@@ -12,13 +13,13 @@ model = sys.argv[0]
 mask = int(sys.argv[1])
 init_cage= int(sys.argv[2])
 if len(sys.argv) == 6:
-    curr_cage = sys.argv[5];
+    curr_cage = int(sys.argv[5]);
 else:
     curr_cage = None
 
 
 # PATHS
-test_path = r'../../codi_enviat/test/elefant/'
+test_path = r'../test/elefant/'
 mask_num = '%(number)02d' % {"number": mask}
 init_cage_name = '%(number)02d' % {"number": init_cage}
 curr_cage_name = '%(number)02d' % {"number": curr_cage}
@@ -28,6 +29,8 @@ init_cage_name = test_path + 'cage_'+init_cage_name+'.txt'
 curr_cage_name = test_path + 'cage_'+curr_cage_name+'.txt'
 
 # LOAD Cage and Mask
-mask_file = open(mask_name, 'r')
-init_cage_file = open(init_cage_name, 'r')
-curr_cage_file = open(curr_cage_name, 'r')
+#mask_file = np.open(mask_name, 'r')
+init_cage_file = np.loadtxt(init_cage_name, float)
+curr_cage_file = np.loadtxt(curr_cage_name, float)
+
+print init_cage_file
