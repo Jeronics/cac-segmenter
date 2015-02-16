2fd65638fe68253dea389bce2a20b2302db9362f
Jeroni <jeronicarandell@gmail.com>
2015-02-16 17:53:08 +0000

    Finish cage create

--- dataset/main_create.py
+++ dataset/main_create.py
@@ -1,17 +1,47 @@
-__author__ = 'jeroni'
 import sys
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
+
+from PyQt5 import Qt
+from PyQt5 import uic
 from PyQt5.QtGui import *
+
+a = Qt.QApplication(sys.argv)
+
+# from untitled import Ui_Form
 
 import numpy as np
 from main_directory import utils
+from PIL import Image
 
 PI = 3.14159265358979323846264338327950288419716939937510
 
+from PyQt5 import QtCore, QtGui, QtWidgets
 
-def create_mask_and_cage_points(c, p, image, num_cage_points):
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Click on the center and radius")
+        image=utils.read_png(IN_FILENAME)
+        print image.shape[0],image.shape[1]
+        Form.resize(image.shape[0],image.shape[1])
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Form.sizePolicy().hasHeightForWidth())
+        Form.setSizePolicy(sizePolicy)
+        self.gridLayout = QtWidgets.QGridLayout(Form)
+        self.gridLayout.setObjectName("gridLayout")
+        self.graphicsView = QtWidgets.QGraphicsView(Form)
+        self.graphicsView.setObjectName("graphicsView")
+        self.gridLayout.addWidget(self.graphicsView, 0, 0, 1, 1)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Click on the center and radius", "Click on the center and radius"))
+
+# Override like this:
+def create_mask_and_cage_points(c, p):
     '''
     This function creates a mask and a sequence of cages.
     :param c:
@@ -20,6 +50,8 @@ def create_mask_and_cage_points(c, p, image, num_cage_points):
     :param num_cage_points:
     :return:
     '''
+    num_cage_points = [8,9,10]
+    image=utils.read_png(IN_FILENAME)
     im_shape = image.shape
     radius = np.linalg.norm(np.array(c) - np.array(p))
     radius_cage_ratio = [1.3, 1.5, 1.7]
@@ -55,36 +87,19 @@ def create_mask_and_cage_points(c, p, image, num_cage_points):
                 text_file.write("%.8e\t%.8e\n" % (y, x))
             cages[str(n) + '_' + str(ratio)] = np.array(cage)
     print cages.keys()
-    utils.plotContourOnImage(np.array(mask_points), image,
-                             points=cages[str(num_cage_points[0]) + '_' + str(radius_cage_ratio[0])],
-                             points2=cages[str(num_cage_points[1]) + '_' + str(radius_cage_ratio[1])])
+    # utils.plotContourOnImage(np.array(mask_points), image,
+    #                          points=cages[str(num_cage_points[0]) + '_' + str(radius_cage_ratio[0])],
+    #                          points2=cages[str(num_cage_points[1]) + '_' + str(radius_cage_ratio[1])])
 
 
-class DrawImage(QMainWindow):
+class override_graphicsScene(Qt.QGraphicsScene):
     def __init__(self, parent=None):
-        super(QMainWindow, self).__init__(parent)
+        super(override_graphicsScene, self).__init__(parent)
 
-        self.setWindowTitle('Select Window')
-        self.local_image = QImage(IN_FILENAME)
-
-        self.local_grview = QGraphicsView()
-        self.setCentralWidget(self.local_grview)
-
-        self.local_scene = QGraphicsScene()
-
-        self.image_format = self.local_image.format()
-        self.pixMapItem = self.local_scene.addPixmap( QPixmap(self.local_image) )
-        # self.pixMapItem = QGraphicsPixmapItem(QPixmap(self.local_image), None, self.local_scene)
-
-        self.local_grview.setScene(self.local_scene)
-
-        self.pixMapItem.mousePressEvent = self.pixelSelect
-
-    def pixelSelect(self, event):
-        print 'hello'
-        position = QPoint(event.pos().x(), event.pos().y())
-        print(str(event.pos().x()))
-        print( str(event.pos().y()) )
+    def mousePressEvent(self, event):
+        super(override_graphicsScene, self).mousePressEvent(event)
+        position = (event.pos().y(), event.pos().x())
+        print position
 
         global COUNTER
         global CENTER
@@ -92,21 +107,41 @@ class DrawImage(QMainWindow):
         global IN_FILENAME
         if COUNTER == 0:
             # The first point is the center
-            CENTER = [event.y, event.x]
+            CENTER = [event.pos().y(), event.pos().x()]
             print 'Center', CENTER
             COUNTER += 1
         else:
             # The second point is a point in the radius
-            RADIUS_POINT = [event.y, event.x]
+            RADIUS_POINT = [event.pos().y(), event.pos().x()]
             print 'RADIUS_POINT', RADIUS_POINT
-            create_mask_and_cage_points(CENTER, RADIUS_POINT, image, num_cage_points)
+            create_mask_and_cage_points(CENTER, RADIUS_POINT)
+
+
+class Image_Process(Qt.QWidget):
+    def __init__(self):
+        super(Image_Process, self).__init__()
+        self.path = IN_FILENAME  # image path
+        print QImage(self.path).size()
+        print QImage(self.path).width(), QImage(self.path).height()
+        self.new = Ui_Form()
+        self.new.setupUi(self)
+
+        self.pixmap = Qt.QPixmap()
+        self.pixmap.load(self.path)
+        self.pixmap = self.pixmap.scaled(self.size(), Qt.Qt.KeepAspectRatio)
+
+        self.graphicsPixmapItem = Qt.QGraphicsPixmapItem(self.pixmap)
+
+        self.graphicsScene = override_graphicsScene(self)
+        self.graphicsScene.addItem(self.graphicsPixmapItem)
+
+        self.new.graphicsView.setScene(self.graphicsScene)
 
 
 def open_canvas():
-    app = QtWidgets.QApplication(sys.argv)
-    form = DrawImage()
-    form.show()
-    app.exec_()
+    my_Qt_Program = Image_Process()
+    my_Qt_Program.show()
+    a.exec_()
 
 
 if __name__ == '__main__':
@@ -123,9 +158,8 @@ if __name__ == '__main__':
         CENTER = []
         RADIUS_POINT = []
         for files in f:
-            if files.split('.')[-1] == 'png':
+            if files.split('.')[-1] == 'png' and files.split("/")[-1].split("_")[0]!='mask':
                 print files
                 IN_FILENAME = r + "/" + files
                 print IN_FILENAME
                 open_canvas()
-
---
+++ dataset/untitled.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'untitled.ui'
+#
+# Created: Mon Jan 12 02:07:05 2015
+#      by: PyQt5 UI code generator 5.3.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Click on the center and radius")
+        Form.resize(600, 60)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Form.sizePolicy().hasHeightForWidth())
+        Form.setSizePolicy(sizePolicy)
+        self.gridLayout = QtWidgets.QGridLayout(Form)
+        self.gridLayout.setObjectName("gridLayout")
+        self.graphicsView = QtWidgets.QGraphicsView(Form)
+        self.graphicsView.setObjectName("graphicsView")
+        self.gridLayout.addWidget(self.graphicsView, 0, 0, 1, 1)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Click on the center and radius", "Click on the center and radius"))
