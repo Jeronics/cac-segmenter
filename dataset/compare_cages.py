1acdcf357da47afe16fb78d37cbbcb92583ac3ea
Jeroni <jeronicarandell@gmail.com>
2015-03-09 20:58:04 +0000

    Add sim.c and makefile for polygon similarity

--- dataset/compare_cages.py
+++ dataset/compare_cages.py
@@ -2,10 +2,23 @@ __author__ = 'jeroni'
 import numpy as np
 import math
 from sympy import Polygon, Point
+from ctypes import *
+import numpy as np
+
+libcac = CDLL("pol_sym/libcac.so")
+
+cac_contour_get_interior_contour = libcac.cac_contour_get_interior_contour
+cac_get_affine_coordinates = libcac.cac_get_affine_coordinates
+cac_get_omega1_omega2 = libcac.cac_get_omega1_omega2
+
+LP_c_int = POINTER(c_int)
+LP_c_double = POINTER(c_double)
+LP_LP_c_double = POINTER(LP_c_double)
+
 
 p1 = Point(0, 0)
 p2 = Point(1, 0)
-p3 = Point(5, 1)
+p3 = Point(1, 1)
 p4 = Point(0, 1)
 
 x = Polygon(p1, p2, p3, p4)
@@ -21,7 +34,7 @@ def get_angle(input_val):
     if len(prod) > 1:
         div = prod[1].split('/')
         root = div[0].replace('sqrt(', '').replace(')', '')
-        angle = math.pow(np.cos(int(prod[0]) * np.sqrt(int(root)) / float(int(div[1]))), 2)
+        angle = math.pow(np.cos(int(prod[0]) * np.sqrt(int(root)) / float(int(div[1]))), -1)
     else:
         if 'pi/' in str_val:
             denom = str_val.replace('pi/', '')
