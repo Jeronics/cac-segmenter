e6a51eca5aea094a5d48d815842d1d9d67c2b65b
Jeroni <jeronicarandell@gmail.com>
2015-03-11 00:06:35 +0000

    Finished Turning function

--- dataset/turning_function.py
+++ dataset/turning_function.py
@@ -8,8 +8,7 @@ from matplotlib import pyplot as plt
 
 def multiple_euclid_dist(p1, p2):
     dis = p1 - p2
-    return [np.sqrt(np.dot(x,x)) for x in dis]
-
+    return [np.sqrt(np.dot(x, x)) for x in dis]
 
 
 def dotproduct(v1, v2):
@@ -18,6 +17,7 @@ def dotproduct(v1, v2):
 
 def length(v):
     return math.sqrt(dotproduct(v, v))
+
 
 def multiple_angles(v_list, vector):
     '''
@@ -30,13 +30,12 @@ def multiple_angles(v_list, vector):
     return [angle(v, vector) for v in v_list]
 
 
-
 def angle(v1, v2):
-    print v1,v2
-    if np.sin(dotproduct(v1, v2) / (length(v1) * length(v2))) ==1.0:
-        return np.pi/2.
+    print v1, v2
+    if np.sin(dotproduct(v1, v2) / (length(v1) * length(v2))) == 1.0:
+        return np.pi / 2.
     elif np.sin(dotproduct(v1, v2) / (length(v1) * length(v2))) == -1.0:
-        return np.pi*3/2.
+        return np.pi * 3 / 2.
     else:
         return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
 
@@ -45,24 +44,41 @@ def radiants_to_degrees(radiant):
     return radiant * 180 / np.pi
 
 
-def plot_discontinuous_funct(x,y, display=True):
-    def heaviside(x):
-        """See http://stackoverflow.com/a/15122658/554319"""
-        y = 0.5 * (np.sign(x) + 1)
-        y[np.diff(y) >= 0.5] = np.nan
-        return y
+# def plot_discontinuous_funct(x,y, display=True):
+# def heaviside(x):
+# """See http://stackoverflow.com/a/15122658/554319"""
+# y = 0.5 * (np.sign(x) + 1)
+#         y[np.diff(y) >= 0.5] = np.nan
+#         return y
+#
+#     def U(x, n):
+#         aux= sum([heaviside(x - 1 / float(i)) / i ** 2 for i in range(1, n)])
+#         print aux
+#         return aux
+#
+#     N = 1000
+#     x = np.linspace(0, 1, N)
+#     plt.plot(x, U(x, 50))
+#     if display:
+#         plt.show()
 
-    def U(x, n):
-        return sum([heaviside(x - 1 / float(i)) / i ** 2 for i in range(1, n)])
+
+def plot_discontinuous_funct(val, f_val, display=True):
+    def get_value(x):
+        for a, b in zip(val, f_val):
+            if x <= a:
+                return b
+    def U(x):
+        return [get_value(i) for i in x]
 
     N = 1000
-    x = np.linspace(0, 1, N)
-    print x
-    plt.plot(x, U(x, 50))
+    x = np.linspace(0, val[-1], N)
+    print len(x), len(U(x))
+    plt.plot(x, U(x))
     if display:
+        plt.xlim(0, val[-1])
+        print 'value', val[-1]
         plt.show()
-
-
 
 
 def turning_funcion(p):
@@ -71,9 +87,13 @@ def turning_funcion(p):
     vectors = p - q
     x_axis = np.array([1, 0])
     angles = multiple_angles(vectors, x_axis)
-    print len(multiple_euclid_dist(p,q))
+    x = multiple_euclid_dist(p, q)
+    print x
+    x = np.cumsum(x)
+    x=x/float(x[-1])
+    print x
     print len(angles)
-    plot_discontinuous_funct(multiple_euclid_dist(p,q), angles)
+    plot_discontinuous_funct(x, angles)
     plt.show()
 
     return 0
@@ -82,12 +102,9 @@ def turning_funcion(p):
 if __name__ == '__main__':
     poly_1 = np.array([
         [0, 0],
-        [0, 1],
-        [1, 1],
+        [0, 2],
         [1, 0]
     ])
     turning_funcion(poly_1)
-    print angle([0, 1], [1, 1])
-    print radiants_to_degrees(angle([0, 1], [0, 1]))
 
 
