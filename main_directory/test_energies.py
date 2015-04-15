38327abaae04629c6cb62024430120f7ebd9c2db
Jeroni <jeronicarandell@gmail.com>
2015-04-15 15:18:04 +0000

    Finished testing vertex constraint energy and its gradient

--- main_directory/test_energies.py
+++ main_directory/test_energies.py
@@ -8,13 +8,12 @@ class TestEnergies(unittest.TestCase):
     '''
 
     '''
+
     def setUp(self):
         '''
 
         :return:
         '''
-        # Case 1
-
         '''
         case 1: Only two points influence each other
 
@@ -36,47 +35,46 @@ class TestEnergies(unittest.TestCase):
         energy1 = 81
 
         '''
-        case 2: equilateral triangle
+        case 2: Triangle with 45 a deg. angle at (0,0)
 
-                         * (2,2*sqrt(2)
+                           * (2*sqrt(2),2*sqrt(2))
 
-                (0,0) *     * (4,0)
+                (0,0) *        * (4*sqrt(2),0)
 
         '''
         vertices2 = np.array([
             [0, 0],
-            [2, 2*np.sqrt(3)],
-            [4, 0]
+            [2 * np.sqrt(2), 2 * np.sqrt(2)],
+            [4 * np.sqrt(2), 0]
         ])
         d2 = 10
-        dist_1_2=2*(10-2*np.sqrt(3)/(2*np.sqrt(3)-10)
         gradients2 = np.array([
-            [-np.sqrt(12)-12, -np.sqrt(12)],
-            [np.sqrt(12), np.sqrt(12)],
-            [12+*2), 0]
+            [-20 + 2 * np.sqrt(2), -6 * np.sqrt(2)],
+            [0, 12 * np.sqrt(2)],
+            [20 - 2 * np.sqrt(2), -6 * np.sqrt(2)]
         ])
-        energy2 = 81
+        energy2 = 36 * 2 + np.power(10 - 4 * np.sqrt(2), 2)
 
         '''
-            Case 3: Slanted Isosceles triangle
+            Case 3: Equilateral triangle
 
-                      * (2*sqrt(2),2*sqrt(2))
+                     * (2,2*sqrt(3))
 
             (0,0) *     * (4,0)
 
         '''
         vertices3 = np.array([
             [0, 0],
-            [2*np.sqrt(2), 2*np.sqrt(2)],
+            [2, 2 * np.sqrt(3)],
             [4, 0]
         ])
         d3 = 10
         gradients3 = np.array([
-            [0, 18],
-            [0, -18],
-            [0, 0]
+            [-18, -6 * np.sqrt(3)],
+            [0, 12 * np.sqrt(3)],
+            [18, -6 * np.sqrt(3)]
         ])
-        energy3 = 81
+        energy3 = 36 * 3
 
         '''
 
@@ -98,10 +96,11 @@ class TestEnergies(unittest.TestCase):
 
         self.expected_values = (
             ((vertices1, d1), gradients1, energy1),
-            ((vertices1+1212, d1), gradients1, energy1),
-            # ((vertices2, d2), gradients2, energy2),
+            ((vertices1 + 1212, d1), gradients1, energy1),
+            ((vertices2, d2), gradients2, energy2),
             ((vertices3, d3), gradients3, energy3),
             ((vertices4, d4), gradients4, energy4),
+
         )
 
     def test_vertex_constraint_grad(self):
@@ -114,18 +113,18 @@ class TestEnergies(unittest.TestCase):
             print '\n'
             print predicted
             print expected
-            print predicted==expected
-            print np.linalg.norm(predicted-expected)
-            self.assertEqual(np.linalg.norm(predicted-expected) < 0.0001, True)
+            # print np.linalg.norm(predicted - expected)
+            self.assertEqual(np.linalg.norm(predicted - expected) < 0.0001, True)
 
-    # def test_vertex_constraint(self):
-    #     '''
-    #     Tests whether the vertex_constraint function works.
-    #     :return:
-    #     '''
-    #     for input_vars, _, expected in self.expected_values:
-    #         predicted = energies.vertex_constraint(*input_vars)
-    #         self.assertEqual(predicted, expected)
+            # def test_vertex_constraint(self):
+            # '''
+            # Tests whether the vertex_constraint function works.
+            # :return:
+            # '''
+            # for input_vars, _, expected in self.expected_values:
+            # predicted = energies.vertex_constraint(*input_vars)
+            # self.assertEqual(predicted, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
