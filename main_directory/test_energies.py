e5ca3031df63e815813099267183467da8932e1e
Jeroni <jeronicarandell@gmail.com>
2015-04-20 17:01:00 +0000

    Finished testing mean color in region

--- main_directory/test_energies.py
+++ main_directory/test_energies.py
@@ -248,6 +248,7 @@ class TestColorMean(unittest.TestCase):
         h_red = 0
         s_red = 255.
         i_red = 85
+        mean_red = 0
 
         # Green
         col = color1.copy()
@@ -259,6 +260,7 @@ class TestColorMean(unittest.TestCase):
         h_green = 2 * np.pi / 3.
         s_green = np.sqrt(np.power(255 / 2., 2) + 3 * np.power(255 / 2., 2))
         i_green = 85
+        mean_green = 2 * np.pi / 3.
 
         # Blue
         col = color1.copy()
@@ -270,6 +272,7 @@ class TestColorMean(unittest.TestCase):
         h_blue = 4 * np.pi / 3.
         s_blue = np.sqrt(np.power(255 / 2., 2) + 3 * np.power(255 / 2., 2))
         i_blue = 85
+        mean_blue = 4 * np.pi / 3.
 
         # Yellow
         col = color1.copy()
@@ -281,6 +284,7 @@ class TestColorMean(unittest.TestCase):
         h_yellow = np.pi / 3.
         s_yellow = np.sqrt(np.power(255 - 255 / 2., 2) + 3 * np.power(255 / 2., 2))
         i_yellow = 170
+        mean_yellow = np.pi / 3.
 
         # Pink
         col = color1.copy()
@@ -292,6 +296,7 @@ class TestColorMean(unittest.TestCase):
         h_pink = 5 * np.pi / 3.
         s_pink = np.sqrt(np.power(255 - 255 / 2., 2) + 3 * np.power(255 / 2., 2))
         i_pink = 170
+        mean_pink = 5 * np.pi / 3.
 
         # Clear Blue
         col = color1.copy()
@@ -303,6 +308,7 @@ class TestColorMean(unittest.TestCase):
         h_clear_blue = np.pi
         s_clear_blue = 255.
         i_clear_blue = 170
+        mean_clear_blue = np.pi
 
         # Black
         col = color1.copy()
@@ -314,6 +320,7 @@ class TestColorMean(unittest.TestCase):
         h_black = 0
         s_black = 0
         i_black = 0
+        mean_black = 0
 
         # White
         col = color1.copy()
@@ -325,28 +332,68 @@ class TestColorMean(unittest.TestCase):
         h_white = 0
         s_white = 0
         i_white = 255
+        mean_white = 0
+
+        # Red-Yellow-black-white
+        col = color1.copy()
+
+        col[:, :5, 0], col[:, :5, 1], col[:, :5, 2] = 255., 0., 0.
+        col[:, 5:10, 0], col[:, 5:10, 1], col[:, 5:10, 2] = 255., 255., 0
+        col[:, 10:15, 0], col[:, 10:15, 1], col[:, 10:15, 2] = 0., 0., 0.
+        col[:, 15:20, 0], col[:, 15:20, 1], col[:, 15:20, 2] = 255., 255., 255.
+        mix1_coordinates = np.array([[i, j] for i in np.arange(col.shape[0]) for j in np.arange(col.shape[1])])
+        im_mix1 = utils.ImageClass(col)
+        h_mix1 = None  # Should be a vector we do not use it for our case
+        s_mix1 = None  # Idem.
+        i_mix1 = None  # Idem.
+        mean_mix1 = np.pi / 6.
 
         self.expected_values = (
-            ((red_coordinates, im_red.image), h_red, s_red, i_red),
-            ((green_coordinates, im_green.image), h_green, s_green, i_green),
-            ((blue_coordinates, im_blue.image), h_blue, s_blue, i_blue),
-            ((yellow_coordinates, im_yellow.image), h_yellow, s_yellow, i_yellow),
-            ((pink_coordinates, im_pink.image), h_pink, s_pink, i_pink),
-            ((clear_blue_coordinates, im_clear_blue.image), h_clear_blue, s_clear_blue, i_clear_blue),
-            ((black_coordinates, im_black.image), h_black, s_black, i_black),
-            ((white_coordinates, im_white.image), h_white, s_white, i_white),
+            ((red_coordinates, im_red.image), h_red, s_red, i_red, mean_red),
+            ((green_coordinates, im_green.image), h_green, s_green, i_green, mean_green),
+            ((blue_coordinates, im_blue.image), h_blue, s_blue, i_blue, mean_blue),
+            ((yellow_coordinates, im_yellow.image), h_yellow, s_yellow, i_yellow, mean_yellow),
+            ((pink_coordinates, im_pink.image), h_pink, s_pink, i_pink, mean_pink),
+            ((clear_blue_coordinates, im_clear_blue.image), h_clear_blue, s_clear_blue, i_clear_blue, mean_clear_blue),
+            ((black_coordinates, im_black.image), h_black, s_black, i_black, mean_black),
+            ((white_coordinates, im_white.image), h_white, s_white, i_white, mean_white),
+        )
+        self.expected_mix_mean = (
+            ((mix1_coordinates, im_mix1.image), mean_mix1),
         )
 
     def test_rgb_to_green(self):
         '''
         Tests whether the rgb_to_hsi function works.
         '''
-        for input_vars, h, s, i in self.expected_values:
+        for input_vars, h, s, i, m in self.expected_values:
             pred_h, pred_s, pred_i = energies.rgb_to_hsi(*input_vars)
-            print h, s, i
             self.assertEqual(np.linalg.norm(pred_h - h) < 0.0001, True)
             self.assertEqual(np.linalg.norm(pred_s - s) < 0.0001, True)
             self.assertEqual(np.linalg.norm(pred_i - i) < 0.0001, True)
+
+    def test_mean_color_in_region_on_homogeneously_colored_surfaces(self):
+        '''
+        Tests whether the mean_color_in_region function works.
+        '''
+        for input_vars, _, _, _, expected_angle in self.expected_values:
+            predicted_angle = energies.mean_color_in_region(*input_vars)
+            print expected_angle, predicted_angle
+            self.assertEqual(np.linalg.norm(expected_angle - predicted_angle) < 0.0001, True)
+
+    def test_mean_color_in_region_on_mixed_colored_surface(self):
+        '''
+        Tests whether the mean_color_in_region function works on mixed colours and correctly ignores black and white.
+        :return:
+        '''
+        for input_vars, expected_mean_angle in self.expected_mix_mean:
+            predicted_mean_angle = energies.mean_color_in_region(*input_vars)
+            self.assertEqual(np.linalg.norm(expected_mean_angle - predicted_mean_angle) < 0.0001, True)
+    #
+    # def test_distance_between_colors(self):
+    #     '''
+    #     :return:
+    #     '''
 
 
 if __name__ == '__main__':
