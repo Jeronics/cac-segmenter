87aadcfdbd2523c43721ce2596c4955187b072f2
Jeroni <jeronicarandell@gmail.com>
2015-06-08 17:15:20 +0000

    Begin changing the grad_mean_color_energy_per_region

--- main_directory/energies.py
+++ main_directory/energies.py
@@ -117,23 +117,43 @@ def get_omega_mean(omega_coord, image):
                         Multi Dimensional MEAN ENERGY
 '''
 
-def generic_mean_energy_per_region(omega_1_coord, affine_omega_1_coord, image, type):
-    return None
+
+def generic_mean_energy_per_region(omega_coord, affine_omega_coord, image_obj, type, slice):
+    if type == 'C':
+        omega_energy = mean_color_energy_per_region(omega_coord, image_obj)
+    if type == 'N':
+        image = image_obj.image[:, :, slice]
+        omega_energy = mean_energy_per_region(omega_coord, affine_omega_coord, image)
+    return omega_energy
+
 
 def mean_energy_multi(omega_1_coord, omega_2_coord, affine_omega_1_coord, affine_omega_2_coord, image, type, weight):
-    energy = []
-    for t,w in zip(type, weight):
-        omega_1 = mean_energy_per_region(omega_1_coord, affine_omega_1_coord, image, )
-        omega_2 = mean_energy_per_region(omega_2_coord, affine_omega_2_coord, image)
+    total_energy = []
+    for slice, (t, w) in enumerate(zip(type, weight)):
+        omega_1 = generic_mean_energy_per_region(omega_1_coord, affine_omega_1_coord, image, t, slice)
+        omega_2 = generic_mean_energy_per_region(omega_2_coord, affine_omega_2_coord, image, t, slice)
         energy = (omega_1 + omega_2) / float(2)
+        total_energy.append(w * energy)
+    return total_energy / float(len(total_energy))
 
-    return energy
+
+def generic_grad_mean_energy_per_region(omega_coord, affine_omega_coord, image_obj, type, slice):
+    if type == 'C':
+        omega_energy = grad_mean_color_energy_per_region(omega_coord, affine_omega_coord, image_obj)
+    if type == 'N':
+        image = image_obj.image[:, :, slice]
+        omega_energy = grad_mean_color_energy_per_region(omega_coord, affine_omega_coord, image)
 
 
-def mean_energy_per_region_multi(omega_coord, affine_omega_coord, image):
-    omega_mean, omega_std = get_omega_mean(omega_coord, image)
-    aux = utils.evaluate_image(omega_coord, image, omega_mean) - omega_mean
-    return np.dot(aux, np.transpose(aux))
+def mean_energy_grad_multi(omega_1_coord, omega_2_coord, affine_omega_1_coord, affine_omega_2_coord, image, type,
+                           weight):
+    total_grad_energy=[]
+    for slice, (t, w) in enumerate(zip(type, weight)):
+        grad_energy_1 = grad_mean_color_energy_per_region(omega_1_coord, affine_omega_1_coord, image, t, slice)
+        grad_energy_2 = grad_mean_color_energy_per_region(omega_2_coord, affine_omega_2_coord, image, t, slice)
+        grad_energy = grad_energy_1 + grad_energy_2
+        total_grad_energy.append(w*grad_energy)
+    return
 
 
 '''
