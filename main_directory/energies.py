ac96605f7745aef2b165900ae7ab5766cff1edb0
Jeroni <jeronicarandell@gmail.com>
2015-06-09 15:06:35 +0000

    Finish generic grad_mean_energy_per_region

--- main_directory/energies.py
+++ main_directory/energies.py
@@ -142,17 +142,18 @@ def generic_grad_mean_energy_per_region(omega_coord, affine_omega_coord, image_o
         omega_energy = grad_mean_color_energy_per_region(omega_coord, affine_omega_coord, image_obj)
     if type == 'N':
         image = image_obj.image[:, :, slice]
-        omega_energy = grad_mean_color_energy_per_region(omega_coord, affine_omega_coord, image)
+        image_gradient = np.array(np.gradient(image))
+        omega_energy = mean_energy_grad_per_region(omega_coord, affine_omega_coord, image, image_gradient)
 
 
 def mean_energy_grad_multi(omega_1_coord, omega_2_coord, affine_omega_1_coord, affine_omega_2_coord, image, type,
                            weight):
-    total_grad_energy=[]
+    total_grad_energy = []
     for slice, (t, w) in enumerate(zip(type, weight)):
         grad_energy_1 = grad_mean_color_energy_per_region(omega_1_coord, affine_omega_1_coord, image, t, slice)
         grad_energy_2 = grad_mean_color_energy_per_region(omega_2_coord, affine_omega_2_coord, image, t, slice)
         grad_energy = grad_energy_1 + grad_energy_2
-        total_grad_energy.append(w*grad_energy)
+        total_grad_energy.append(w * grad_energy)
     return
 
 
