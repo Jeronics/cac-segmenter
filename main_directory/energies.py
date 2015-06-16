a1b0ead6c8d306dea74ff363d16e46c65bf51823
Jeroni <jeronicarandell@gmail.com>
2015-06-16 15:29:59 +0000

    Add ege constraint and others

--- main_directory/energies.py
+++ main_directory/energies.py
@@ -111,52 +111,6 @@ def get_omega_mean(omega_coord, image):
     omega_std = np.std(image[[omega_coord_aux[:, 0].tolist(), omega_coord_aux[:, 1].tolist()]])
     return omega_mean, omega_std
 
-
-'''
-                        Multi Dimensional MEAN ENERGY
-'''
-
-
-def generic_mean_energy_per_region(omega_coord, affine_omega_coord, image_obj, type, slice):
-    if type == 'C':
-        omega_energy = mean_color_energy_per_region(omega_coord, image_obj)
-    if type == 'N':
-        image = image_obj.image[:, :, slice]
-        omega_energy = mean_energy_per_region(omega_coord, affine_omega_coord, image)
-    return omega_energy
-
-
-def mean_energy_multi(omega_1_coord, omega_2_coord, affine_omega_1_coord, affine_omega_2_coord, image, type, weight):
-    total_energy = []
-    for slice, (t, w) in enumerate(zip(type, weight)):
-        omega_1 = generic_mean_energy_per_region(omega_1_coord, affine_omega_1_coord, image, t, slice)
-        omega_2 = generic_mean_energy_per_region(omega_2_coord, affine_omega_2_coord, image, t, slice)
-        energy = (omega_1 + omega_2) / float(2)
-        total_energy.append(w * energy)
-    return sum(total_energy)
-
-
-def generic_grad_mean_energy_per_region(omega_coord, affine_omega_coord, image_obj, type, slice):
-    if type == 'C':
-        omega_energy = grad_mean_color_energy_per_region(omega_coord, affine_omega_coord, image_obj)
-    if type == 'N':
-        image = image_obj.image[:, :, slice]
-        image_gradient = np.array(np.gradient(image))
-        omega_energy = mean_energy_grad_per_region(omega_coord, affine_omega_coord, image, image_gradient)
-    return omega_energy
-
-
-def mean_energy_grad_multi(omega_1_coord, omega_2_coord, affine_omega_1_coord, affine_omega_2_coord, image, type,
-                           weight):
-    total_grad_energy = []
-    for slice, (t, w) in enumerate(zip(type, weight)):
-        grad_energy_1 = grad_mean_color_energy_per_region(omega_1_coord, affine_omega_1_coord, image, t, slice)
-        grad_energy_2 = grad_mean_color_energy_per_region(omega_2_coord, affine_omega_2_coord, image, t, slice)
-        grad_energy = grad_energy_1 + grad_energy_2
-        total_grad_energy.append(w * grad_energy)
-    return sum(total_grad_energy)
-
-
 '''
 
                         CONSTRAINT ENERGIES
