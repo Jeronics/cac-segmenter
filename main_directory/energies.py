1c155f22cd45b1092b7c00516ed3239363ac3d9c
Jeroni <jeronicarandell@gmail.com>
2015-06-16 15:39:14 +0000

    Fix imports in energy utils mean

--- main_directory/energies.py
+++ main_directory/energies.py
@@ -49,68 +49,6 @@ def second_step_alpha(alpha, curr_cage, grad_k, band_size, affine_contour_coord,
         return 0
     return 1
 
-
-'''
-
-                        MEAN ENERGY
-
-'''
-
-
-def mean_energy(omega_1_coord, omega_2_coord, affine_omega_1_coord, affine_omega_2_coord, image_obj):
-    image = image_obj.gray_image
-    omega_1 = mean_energy_per_region(omega_1_coord, affine_omega_1_coord, image)
-    omega_2 = mean_energy_per_region(omega_2_coord, affine_omega_2_coord, image)
-    energy = (omega_1 + omega_2) / float(2)
-    return energy
-
-
-def mean_energy_grad(omega1_coord, omega2_coord, affine_omega_1_coord, affine_omega_2_coord, image_obj):
-    # Calculate Image gradient
-    image = image_obj.gray_image
-    image_gradient = np.array(np.gradient(image))
-
-    # Calculate Energy:
-    omega_1 = mean_energy_grad_per_region(omega1_coord, affine_omega_1_coord, image, image_gradient)
-    omega_2 = mean_energy_grad_per_region(omega2_coord, affine_omega_2_coord, image, image_gradient)
-    energy = omega_1 + omega_2
-    return energy
-
-
-def mean_energy_per_region(omega_coord, affine_omega_coord, image):
-    omega_mean, omega_std = get_omega_mean(omega_coord, image)
-    aux = utils.evaluate_image(omega_coord, image, omega_mean) - omega_mean
-    return np.dot(aux, np.transpose(aux))
-
-
-def mean_energy_grad_per_region(omega_coord, affine_omega_coord, image, image_gradient):
-    # E_mean
-    omega_mean, omega_std = get_omega_mean(omega_coord, image)
-    aux = utils.evaluate_image(omega_coord, image, omega_mean) - omega_mean
-    image_gradient_by_point = [utils.evaluate_image(omega_coord, image_gradient[0], 0),
-                               utils.evaluate_image(omega_coord, image_gradient[1], 0)]
-    mean_derivative = np.dot(image_gradient_by_point, affine_omega_coord) / float(len(omega_coord))
-    grad = gradient_energy_for_each_vertex(aux, affine_omega_coord, image_gradient_by_point, mean_derivative)
-    return grad  # *(1/pow(omega_std, 2)) for GAUSS
-
-
-def gradient_energy_for_each_vertex(aux, affine_omega_coord, image_gradient_by_point, mean_derivative):
-    # image_gradient_by_point = np.transpose(image_gradient_by_point)
-    aux_x = np.multiply(np.transpose(affine_omega_coord), image_gradient_by_point[0])
-    aux_y = np.multiply(np.transpose(affine_omega_coord), image_gradient_by_point[1])
-    aux_x = np.dot(aux, np.transpose(aux_x) - mean_derivative[0])
-    aux_y = np.dot(aux, np.transpose(aux_y) - mean_derivative[1])
-    aux_1 = np.transpose([aux_x, aux_y])
-    return aux_1
-
-def get_omega_mean(omega_coord, image):
-    omega_boolean = utils.are_inside_image(omega_coord, image.shape)
-    omega_coord_aux = omega_coord[omega_boolean]
-    omega_intensity = sum(image[[omega_coord_aux[:, 0].tolist(), omega_coord_aux[:, 1].tolist()]])
-    omega_mean = omega_intensity / (len(omega_boolean[omega_boolean]))
-    omega_std = np.std(image[[omega_coord_aux[:, 0].tolist(), omega_coord_aux[:, 1].tolist()]])
-    return omega_mean, omega_std
-
 '''
 
                         CONSTRAINT ENERGIES
