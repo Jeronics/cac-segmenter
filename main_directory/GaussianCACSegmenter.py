b0dcff3d93be667ae0ba287f333a4c33e05a475b
Jeroni <jeronicarandell@gmail.com>
2015-07-22 16:31:38 +0000

    Add groundtruth in GaussianCAC seed and init

--- main_directory/GaussianCACSegmenter.py
+++ main_directory/GaussianCACSegmenter.py
@@ -6,8 +6,8 @@ import utils
 
 
 class GaussianCACSegmenter(CAC):
-    def __init__(self, image_obj, mask_obj, cage_obj, type=None, weight=None, band_size=500):
-        CAC.__init__(self, image_obj, mask_obj, cage_obj, type=type, weight=weight, band_size=band_size)
+    def __init__(self, image_obj, mask_obj, cage_obj, ground_truth_obj, type=None, weight=None, band_size=500):
+        CAC.__init__(self, image_obj, mask_obj, cage_obj,  ground_truth_obj, type=type, weight=weight, band_size=band_size)
         inside_seed_mean, inside_seed_std, outside_seed_mean, outside_seed_std = g_energies.initialize_seed(self)
         print 'STANDARDIZE:', self.standardize
         self.inside_seed_mean = inside_seed_mean
