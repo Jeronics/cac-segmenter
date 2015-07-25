ea72ab9ddfaf2dc100f245dce4590e1e0cc6f2ca
Jeroni <jeronicarandell@gmail.com>
2015-07-25 18:00:37 +0000

    Fix compare results

--- main_directory/segment_results_alpert/compare_results.py
+++ main_directory/segment_results_alpert/compare_results.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import os
-
+import pprint
 if __name__ == '__main__':
     folder_name = os.getcwd()+'/'
     for r, s, f in os.walk(folder_name):
         for sub in s:
             print sub
             sorensen_dice_file = folder_name + sub + '/' + 'sorensen_dice_coeff.txt'
-            sorensen_dice = pd.DataFrame.from_csv(sorensen_dice_file, header=0, sep='\t')
-            print sorensen_dice.mean()
+            if os.path.exists(sorensen_dice_file):
+                sorensen_dice = pd.read_csv(sorensen_dice_file, index_col=0, sep='\t', header=None)
+                # pprint.pprint(sorensen_dice),
+                print sorensen_dice.mean(), sorensen_dice.std()
