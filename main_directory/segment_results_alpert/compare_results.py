d320d40324367873ac75d6c11a5e7658bce32b5e
Jeroni <jeronicarandell@gmail.com>
2015-07-26 17:18:24 +0000

    Fix results

--- main_directory/segment_results_alpert/compare_results.py
+++ main_directory/segment_results_alpert/compare_results.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import os
 import pprint
+
 if __name__ == '__main__':
-    folder_name = os.getcwd()+'/'
+    folder_name = os.getcwd() + '/'
+    first = None
     for r, s, f in os.walk(folder_name):
         for sub in s:
             print sub
@@ -10,4 +12,16 @@ if __name__ == '__main__':
             if os.path.exists(sorensen_dice_file):
                 sorensen_dice = pd.read_csv(sorensen_dice_file, index_col=0, sep='\t', header=None)
                 # pprint.pprint(sorensen_dice),
-                print sorensen_dice.mean(), sorensen_dice.std()
+                # Remove duplicates
+                grouped = sorensen_dice.groupby(level=0)
+                sorensen_dice = grouped.last()
+                sorensen_dice.columns = [sub]
+                if not first:
+                    df = sorensen_dice
+                else:
+                    df = df.join(sorensen_dice, how='inner')
+                print 'Num inst.', len(sorensen_dice)
+                print 'Mean', sorensen_dice.mean().values[0]
+                print 'STD', sorensen_dice.std().values[0]
+                print '\n'
+
