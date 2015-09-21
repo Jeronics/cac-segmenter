Fix multivariate gradient

main_directory/MultivariateGaussianCAC.py
@@ -33,7 +33,7 @@ class MultivariateGaussianCAC(CAC):
                                                           image_gradient)
         omega_2 = g_energies.grad_gauss_energy_per_region(omega2_coord, affine_omega_2_coord, self.outside_gmm, image,
                                                           image_gradient)
-        energy = np.sum((omega_1 + omega_2) * self.weight, axis=2)
+        energy = omega_1 + omega_2
         return energy
 
     def _plotContourOnImage(self, contour_coord, cage_obj, alpha, grad_k, color=[0., 0., 255.]):
@@ -45,6 +45,6 @@ if __name__ == '__main__':
     multivariate_gaussian_gray_cac = CACSegmenter(MultivariateGaussianCAC)
     parameter_list = multivariate_gaussian_gray_cac.get_parameters()
 
-    dataset = multivariate_gaussian_gray_cac.load_dataset('AlpertGBB07_input.txt')
-    results_folder = 'segment_results_alpert_prova/' + multivariate_gaussian_gray_cac.CAC.__name__
+    dataset = multivariate_gaussian_gray_cac.load_dataset('synthetic_input.txt')
+    results_folder = 'other/' + multivariate_gaussian_gray_cac.CAC.__name__
     multivariate_gaussian_gray_cac.test_model(dataset, parameter_list[0], results_folder, plot_evolution=True)

main_directory/energy_utils_multivariate_gaussian.py
@@ -54,6 +54,7 @@ def get_values_in_region(omega_coord, image):
     gmm = mixture_gaussian.get_mixture_gaussian(values_in_region, 1, 'full')
     return gmm
 
+
 def gauss_energy_per_region(omega_coord, affine_omega_coord, gmm, image):
     omega_mean, omega_std = gmm.means_, gmm.covars_
     aux = utils.evaluate_image(omega_coord, image, omega_mean) - omega_mean
@@ -71,7 +72,6 @@ def gauss_energy_per_region(omega_coord, affine_omega_coord, gmm, image):
 
 def grad_gauss_energy_per_region(omega_coord, affine_omega_coord, gmm, image, image_gradient, weight=[1, 1, 1]):
     # E_mean
-
     image_gradient_by_point = np.array([utils.evaluate_image(omega_coord, image_gradient[0], 0),
                                         utils.evaluate_image(omega_coord, image_gradient[1], 0)])
     omega_mean, omega_std = gmm.means_, gmm.covars_
@@ -80,8 +80,9 @@ def grad_gauss_energy_per_region(omega_coord, affine_omega_coord, gmm, image, im
     aux = utils.evaluate_image(omega_coord, image, omega_mean)
     aux -= omega_mean
     sigma_aux = np.dot(sigma, aux.T)
-    grad = gradient_gauss_energy_for_each_vertex(sigma_aux, affine_omega_coord, image_gradient_by_point)
-    grad = np.transpose(grad, (2, 0, 1))
+
+    prod_1 = np.sum(sigma_aux.T * np.transpose(image_gradient_by_point, (0, 1, 2)), axis=2)
+    grad = np.dot(prod_1, affine_omega_coord).T
     return grad
 
 