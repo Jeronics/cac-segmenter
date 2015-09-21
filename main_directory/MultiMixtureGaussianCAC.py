Normalize gradient vectors bigger than one. TODO: fix for both steps

main_directory/MultiMixtureGaussianCAC.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.ndimage.filters import gaussian_filter
 
 from CACSegmenter import CACSegmenter
 from CAC import CAC
@@ -11,15 +10,18 @@ class MultiMixtureGaussianCAC(CAC):
     def __init__(self, image_obj, mask_obj, cage_obj, ground_truth_obj, type=None, weight=None, band_size=500):
         CAC.__init__(self, image_obj, mask_obj, cage_obj, ground_truth_obj, type=type, weight=weight,
                      band_size=band_size)
-        inside_gmm, outside_gmm = g_energies.multivariate_initialize_seed(self,maximum_n_components=7)
+        inside_gmm, outside_gmm = g_energies.multivariate_initialize_seed(self, maximum_n_components=7)
+        self.smallest_number = np.exp(-200)
         self.inside_gmm = inside_gmm
         self.outside_gmm = outside_gmm
         self.weight = [1, 1, 1]
 
     def energy(self, omega_1_coord, omega_2_coord, affine_omega_1_coord, affine_omega_2_coord):
         image = self.image_obj.image
-        omega_1 = g_energies.gauss_energy_per_region(omega_1_coord, affine_omega_1_coord, self.inside_gmm, image)
-        omega_2 = g_energies.gauss_energy_per_region(omega_2_coord, affine_omega_2_coord, self.outside_gmm, image)
+        omega_1 = g_energies.gauss_energy_per_region(omega_1_coord, affine_omega_1_coord, self.inside_gmm, image,
+                                                     smallest_num=self.smallest_number)
+        omega_2 = g_energies.gauss_energy_per_region(omega_2_coord, affine_omega_2_coord, self.outside_gmm, image,
+                                                     smallest_num=self.smallest_number)
         energy = -(omega_1 + omega_2) / 2.
         return energy
 
@@ -30,9 +32,9 @@ class MultiMixtureGaussianCAC(CAC):
         image_gradient = np.transpose(image_gradient, (1, 2, 3, 0))
         # Calculate Energy:
         omega_1 = g_energies.grad_gauss_energy_per_region(omega1_coord, affine_omega_1_coord, self.inside_gmm, image,
-                                                          image_gradient)
+                                                          image_gradient, smallest_num=self.smallest_number)
         omega_2 = g_energies.grad_gauss_energy_per_region(omega2_coord, affine_omega_2_coord, self.outside_gmm, image,
-                                                          image_gradient)
+                                                          image_gradient, smallest_num=self.smallest_number)
         energy = np.sum((omega_1 + omega_2) * self.weight, axis=2)
         return energy
 
@@ -40,10 +42,18 @@ class MultiMixtureGaussianCAC(CAC):
         utils.plotContourOnImage(contour_coord, self.image_obj.image, points=current_cage_obj.cage, color=color,
                                  points2=current_cage_obj.cage - alpha * 10 * grad_k)
 
+
 if __name__ == '__main__':
     multi_mixture_gaussian_gray_cac = CACSegmenter(MultiMixtureGaussianCAC)
+    new_parameters = {
+        'num_points': [16],
+        'ratio': [1.05],
+        'smallest_number': [np.exp(-100)],
+    }
+    multi_mixture_gaussian_gray_cac.parameters = new_parameters
+    multi_mixture_gaussian_gray_cac.sigma = 0.25
     parameter_list = multi_mixture_gaussian_gray_cac.get_parameters()
 
     dataset = multi_mixture_gaussian_gray_cac.load_dataset('AlpertGBB07_input_subtest.txt')
-    results_folder = 'segment_subtests/' + multi_mixture_gaussian_gray_cac.CAC.__name__ + '_12_105_075_300/'
-    multi_mixture_gaussian_gray_cac.test_model(dataset, parameter_list[0], results_folder, plot_evolution=False)
+    results_folder = 'other/' + multi_mixture_gaussian_gray_cac.CAC.__name__ + '_16_105_025_100/'
+    multi_mixture_gaussian_gray_cac.test_model(dataset, parameter_list[0], results_folder, plot_evolution=True)

main_directory/compare_results.py
@@ -41,7 +41,9 @@ if __name__ == '__main__':
                 dict_methods[sub] = {
                     'Num_inst': len(sorensen_dice),
                     'Mean': sorensen_dice.mean().values[0],
-                    'STD': sorensen_dice.std().values[0]
+                    'STD': sorensen_dice.std().values[0],
+                    'num_correct':len(sorensen_dice[sorensen_dice[sub]>0.8]),
+                    'mean_correct': sorensen_dice[sorensen_dice[sub]>0.8].mean().values[0]
                 }
 
                 # df = df.fillna(-1)
@@ -85,10 +87,11 @@ if __name__ == '__main__':
     # print np.argmin(final_images_t.mean()), min(final_images_t.max())
 
     # print final
-    print final_images.mean()
-    print len(final_images)
-    filename = 'text_results/subtests_AlpertGBB07/'
-    if not os.path.exists(filename):
-        os.makedirs(filename)
-    final.to_csv(filename + 'subtests.txt')
+    print final
+    print final.sort('mean_correct')
+    # print len(final_images)
+    # filename = 'text_results/subtests_AlpertGBB07/'
+    # if not os.path.exists(filename):
+    #     os.makedirs(filename)
+    # final.to_csv(filename + 'subtests.txt')
     # print len(final_images.values)

main_directory/energy_utils_multimixture_gaussian.py
@@ -50,7 +50,6 @@ def multivariate_initialize_seed(CAC, from_gt=True, maximum_n_components=7, only
     inside_coordinates = np.argwhere(inside_seed == 255.)
     outside_coordinates = np.argwhere(outside_band_seed == 255.)
 
-
     print 'Number of components:'
     inside_gmm = get_values_in_region(inside_coordinates, image, maximum_n_components)
     print 'Interior:\t', inside_gmm.n_components
@@ -67,14 +66,13 @@ def get_values_in_region(omega_coord, image, maximum_n_components=7):
     return gmm
 
 
-def avoid_zero_terms(k):
-    smallest_num = np.exp(-300)
+def avoid_zero_terms(k, smallest_num=np.exp(-200)):
     if np.sum(k < smallest_num) > 0:
         k[k < smallest_num] = k[k < smallest_num] * 0 + smallest_num  # careful with when k[k>0] is empty
     return k
 
 
-def gauss_energy_per_region(omega_coord, affine_omega_coord, gmm, image):
+def gauss_energy_per_region(omega_coord, affine_omega_coord, gmm, image, smallest_num=np.exp(-200)):
     means = np.array([m for m in gmm.means_])
     covars = np.array([v for v in gmm.covars_])
     weights = np.array([w for w in gmm.weights_]).T
@@ -93,12 +91,12 @@ def gauss_energy_per_region(omega_coord, affine_omega_coord, gmm, image):
     coeff = 1 / (np.power(np.sqrt(2 * np.pi), k) * np.sqrt(np.linalg.det(covars)))
     mixt = exp_x_m_denom_x_m * coeff
     mixture_prob = mixt * weights
-    mixture_prob = avoid_zero_terms(mixture_prob)
+    mixture_prob = avoid_zero_terms(mixture_prob, smallest_num=smallest_num)
     energy = np.sum(np.log(mixture_prob))
     return energy
 
 
-def grad_gauss_energy_per_region(omega_coord, affine_omega_coord, gmm, image, image_gradient):
+def grad_gauss_energy_per_region(omega_coord, affine_omega_coord, gmm, image, image_gradient, smallest_num=np.exp(-200)):
     means = np.array([m for m in gmm.means_])
     covars = np.array([v for v in gmm.covars_])
     weights = np.array([w for w in gmm.weights_]).T
@@ -128,7 +126,7 @@ def grad_gauss_energy_per_region(omega_coord, affine_omega_coord, gmm, image, im
 
     mixture_prob = mixture_prob[0, :, :]
 
-    mixture_prob = avoid_zero_terms(mixture_prob)
+    mixture_prob = avoid_zero_terms(mixture_prob, smallest_num=smallest_num)
 
     # caluculate 1/P(x)
     coeff_ = 1 / mixture_prob
@@ -137,8 +135,6 @@ def grad_gauss_energy_per_region(omega_coord, affine_omega_coord, gmm, image, im
     # denom_x_m = np.dot(denom, np.transpose(x_m, (0, 2, 1)))
     # denom_x_m = np.array([denom_x_m[i, :, :, i] for i in xrange(len(weights))])
     denom_x_m = np.array([np.dot(M, X) for M, X in zip(denom, np.transpose(x_m, (1, 2, 0)))])
-
-
 
     unsummed_mixture_derivative = unsummed_mixture_prob * denom_x_m.T / 2.
 

main_directory/mixture_gaussian.py
@@ -21,7 +21,7 @@ def get_number_of_components(X, maximum_n_components=7):
             aux_score = gmm.bic(X)
             bic.append(aux_score)
             # print bic[-1], lowest_bic
-            if bic[-1] < lowest_bic: # and np.min(gmm.weights_) > 0.:
+            if bic[-1] < lowest_bic and np.min(gmm.weights_) > 0.:
                 lowest_bic = bic[-1] - bic[-1] * 0.0
                 best_gmm = gmm
 